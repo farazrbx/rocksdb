5e31621edd78941446057dbda60e256ecf8af7d5
Faraz Fallahi <ffallahi@roblox.com>
2023-11-02 13:33:17 -0700

    Update io_posix.h

--- env/io_posix.h
+++ env/io_posix.h
@@ -29,13 +29,15 @@
 
 // For non linux platform, the following macros are used only as place
 // holder.
-#if !(defined OS_LINUX) && !(defined CYGWIN) && !(defined OS_AIX) && !(defined OS_ANDROID)
+#ifndef POSIX_FADV_NORMAL
 #define POSIX_FADV_NORMAL 0     /* [MC1] no further special treatment */
 #define POSIX_FADV_RANDOM 1     /* [MC1] expect random page refs */
 #define POSIX_FADV_SEQUENTIAL 2 /* [MC1] expect sequential page refs */
 #define POSIX_FADV_WILLNEED 3   /* [MC1] will need these pages */
 #define POSIX_FADV_DONTNEED 4   /* [MC1] don't need these pages */
+#endif
 
+#ifndef POSIX_MADV_NORMAL
 #define POSIX_MADV_NORMAL 0     /* [MC1] no further special treatment */
 #define POSIX_MADV_RANDOM 1     /* [MC1] expect random page refs */
 #define POSIX_MADV_SEQUENTIAL 2 /* [MC1] expect sequential page refs */
