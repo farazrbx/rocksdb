4289dd7a6c5e9e7f07257e5bfb93d63e6c690dce
Faraz Fallahi <ffallahi@roblox.com>
2023-11-02 13:59:50 -0700

    Update port_posix.h

--- port/port_posix.h
+++ port/port_posix.h
@@ -49,7 +49,7 @@
 #include <sys/types.h>
 #define PLATFORM_IS_LITTLE_ENDIAN (_BYTE_ORDER == _LITTLE_ENDIAN)
 #elif defined(__ORBIS__) || defined(__PROSPERO__) // PlayStation 4 and 5
-// PlayStation is little-endian
+#define PLATFORM_IS_LITTLE_ENDIAN true
 #else
 #include <endian.h>
 #endif
